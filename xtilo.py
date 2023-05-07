4fb28b04bfbf912444aa76121d7309b6c2bc4fb8
Xihi <111831296+Xihihhh@users.noreply.github.com>
2023-05-07 23:21:18 +0800

    Update xtilo.py

--- xtilo.py
+++ xtilo.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python3
 
-# Copyright 2017-2020 by YadominJinta. All rights reserved.
-# https://github.com/YadominJinta/atilo has info about the project.
-# https://github.com/YadominJinta/atilo/blob/master/CONTRIBUTORS.md Thank you for help.
+# Copyright 2023-2024 by YadominJinta. All rights reserved.
+# https://github.com/Xihihhh/xtilo has info about the project.
+# https://github.com/Xihihhh/xtilo/blob/master/CONTRIBUTORS.md Thank you for help.
 
 import os
 import tarfile
@@ -14,19 +14,19 @@ import sys
 from tqdm import tqdm
 from prettytable import PrettyTable 
 from bs4 import BeautifulSoup
-
-
 home = os.getenv('HOME')
 atilo_home = home + '/.atilo/'
 atilo_tmp  = atilo_home + 'tmp/'
 atilo_config = atilo_home + 'local.json'
-atilo_version = "2.1.0"
+atilo_version = "2.1.5"
+
 
 def check_dir():
     if not os.path.isdir(atilo_home):
         os.mkdir(atilo_home)
     if not os.path.isdir(atilo_tmp):
         os.mkdir(atilo_tmp)
+
 
 def check_arch():
     arch = os.uname().machine
@@ -43,9 +43,10 @@ def check_arch():
         sys.exit(1)
     return arch
 
+
 def load_local():
     if not os.path.isfile(atilo_config):
-        with open(atilo_config,'w') as f:
+        with open(atilo_config, 'w') as f:
             arch = check_arch()
             data = {
                 'config': {
@@ -53,10 +54,11 @@ def load_local():
                     'version': atilo_version
                 }
             }
-            json.dump(data,indent=4,fp=f)
-    with open(atilo_config,'r') as f:
+            json.dump(data, f, indent=4)
+    with open(atilo_config, 'r') as f:
         config = json.load(f)
     return config
+
 
 def get_list():
     try:
@@ -75,6 +77,7 @@ def get_list():
         sys.exit(1)
     return r.json()
 
+
 def show_list():
     lists = get_list()
     config = load_local()
@@ -84,11 +87,12 @@ def show_list():
     for i in lists.get('linux'):
         name = i
         infos = lists.get(name)
-        version = infos.get ('version')
+        version = infos.get('version')
         installed = name in config.keys()
         installable = arch in infos.keys()
-        table.add_row([name,version,installed,installable])
+        table.add_row([name, version, installed, installable])
     print(table.get_string())
+
 
 def pull_image(distro):
     arch = check_arch()
@@ -105,13 +109,13 @@ def pull_image(distro):
     if arch not in infos.keys():
         print(distro + '不支持该架构')
         sys.exit(1)
-    if infos.get('lxc') == True:
+    if infos.get('lxc'):
         time_stamp = get_lxc(infos.get(arch))
         url = infos.get(arch) + time_stamp + '/rootfs.tar.xz'
     else:
         url = infos.get(arch)
     if os.path.isfile(distro_tmp):
-        print(distro + '已缓存')
+        print('%s 已缓存' % distro)
         print('跳过下载')
     else:
         print('拉取镜像中')
@@ -122,8 +126,8 @@ def pull_image(distro):
             sys.exit(1)
         total_size = int(r.headers.get('Content-Length'))
         block_size = io.DEFAULT_BUFFER_SIZE
-        t = tqdm(total=total_size,unit='iB',unit_scale=True)
-        with open(atilo_tmp + distro,'wb') as f:
+        t = tqdm(total=total_size, unit='iB', unit_scale=True)
+        with open(atilo_tmp + distro, 'wb') as f:
             for chunk in r.iter_content(block_size):
                 t.update(len(chunk))
                 f.write(chunk)
@@ -134,16 +138,16 @@ def pull_image(distro):
         print('跳过校验')
     elif infos.get('check') == 'lxc':
         check_url = infos.get(arch) + time_stamp +'/SHA256SUMS'
-        check_sum(distro=distro,url=check_url,check='sha256')
+        check_sum(distro=distro, url=check_url, check='sha256')
     else:
         check_url = url + '.' + infos.get('check')
-        check_sum(distro=distro,url=check_url,check=infos.get('check'))
-    
+        check_sum(distro=distro, url=check_url, check=infos.get('check'))
     if not infos.get('zip') == 'fedora':
-        extract_file(distro,infos.get('zip'))
+        extract_file(distro, infos.get('zip'))
     else:
         extract_fedora()
-    config_image(distro,infos)
+    config_image(distro, infos)
+
 
 def get_lxc(url):
     r = requests.get(url)
@@ -151,10 +155,11 @@ def get_lxc(url):
         print('无法获取镜像链接')
         print('正在退出')
         sys.exit(1)
-    soup = BeautifulSoup(r.text,'html.parser')
+    soup = BeautifulSoup(r.text, 'html.parser')
     urls = soup.find_all('a')
     time_stamp = (urls[-1]).get('title')
     return time_stamp
+
 
 def remove_image(distro):
     distro_path = atilo_home + distro
@@ -162,48 +167,52 @@ def remove_image(distro):
         print('移除' + distro + '镜像')
         os.system('chmod -R 777 ' + distro_path)
         os.system('rm -rf ' + distro_path)
+        script = atilo_home + 'start-' + distro + '.sh'
+        if os.path.isfile(script):
+            os.system(rm script)
         config = load_local()
         del config[distro]
-        with open(atilo_config,'w') as f:
-            json.dump(config,indent=4,fp=f)
+        with open(atilo_config, 'w') as f:
+            json.dump(config, f, indent=4)
     else:
-        print('未找到' + distro + '镜像')
+        print('未找到 %s 镜像' % distro)
 
 
-def config_image(distro,infos):
+def config_image(distro, infos):
     print('配置镜像中')
     distro_path = atilo_home + distro
     resolv_conf = distro_path + '/etc/resolv.conf'
     if os.path.islink(resolv_conf):
         os.unlink(resolv_conf)
-    with open(resolv_conf,'w') as f:
+    with open(resolv_conf, 'w') as f:
         f.write('nameserver 223.5.5.5\n')
         f.write('nameserver 223.6.6.6\n')
     group = distro_path + '/etc/group'
     with os.popen('whoami') as p:
         userid = p.read()
-        userid = userid[4:len(userid)-1]
+        userid = userid[4 : len(userid) - 1]
         gp1 = '20' + userid
         gp2 = '50' + userid
-    with open(group,'a') as g:
-        g.write("""
+    with open(group, 'a') as g:
+        g.write('''
 3003:x:3003:
 9997:x:9997:
-""" + gp1 + ':x:' + gp1 + """:
-""" + gp2 + ':x:' + gp2 + ':')
+''' + gp1 + ':x:' + gp1 + ''':
+''' + gp2 + ':x:' + gp2 + ':')
     config = load_local()
-    config.update({distro : infos})
-    with open(atilo_config,'w') as f:
-        json.dump(config,indent=4,fp=f)
+    config.update({distro: infos})
+    with open(atilo_config, 'w') as f:
+        json.dump(config, f, indent=4)
     script(distro)
     print('一切完成')
-    print('使用 atilo run ' + distro + ' 来运行')
+    print('使用 atilo run %s 来运行' % distro)
+
 
 def script(distro):
     distro_path = atilo_home + distro
     infos = load_local().get(distro)
     script = atilo_home + 'start-' + distro + '.sh'
-    with open(script,'w') as s:
+    with open(script, 'w') as s:
         s.write('#! /usr/bin/bash\n')
         s.write("""unset LD_PRELOAD
 command='proot'
@@ -232,43 +241,45 @@ command+='""")
         os.system('chmod +x ' + script)
         print('启动脚本已生成')
 
-def extract_file(distro,zip_m):
+
+def extract_file(distro, zip_m):
     distro_path = atilo_home + distro
     file_path = atilo_tmp + distro
     if os.path.isdir(distro_path):
         os.system('chmod -R 777 ' + distro_path)
         os.system('rm -rf ' + distro_path)
-
-    zip_f = tarfile.open(file_path,'r:' + zip_m)
+    zip_f = tarfile.open(file_path, 'r:' + zip_m)
     if not os.path.isdir(distro_path):
         os.mkdir(distro_path)
     print('解压镜像中')
-    zip_f.extractall(distro_path,numeric_owner=True)
+    zip_f.extractall(distro_path, numeric_owner=True)
+
 
 def extract_fedora():
-    file_path = atilo_tmp+ 'fedora'
+    file_path = atilo_tmp + 'fedora'
     distro_path = atilo_home + 'fedora'
     print('解压镜像中')
     zip_f = tarfile.open(file_path)
     for i in zip_f.getnames():
         if 'layer.tar' in i:
             zip_name = i
-    zip_f.extract(zip_name,atilo_tmp)
+    zip_f.extract(zip_name, atilo_tmp)
     zip_f.close()
-    zip_f = tarfile.open(atilo_tmp + zip_name,'r')
+    zip_f = tarfile.open(atilo_tmp + zip_name, 'r')
     if not os.path.isdir(distro_path):
         os.mkdir(distro_path)
-    zip_f.extractall(distro_path,numeric_owner=True)
+    zip_f.extractall(distro_path, numeric_owner=True)
+
 
 def check_sum(distro,url,check):
     print('校验文件完整性')
     r = requests.get(url)
     file_path = atilo_tmp + distro
     if not r.status_code == 200:
-        print('无法获取文件校验码，是否继续 [y/N]',end=' ')
+        print('无法获取文件校验码，是否继续 [y/n]', end=' ')
         a = ''
         input(a)
-        if a not in ('y','Y'):
+        if a not in ('y', 'Y'):
             print('正在退出')
             os.remove(file_path)
             sys.exit(1)
@@ -277,14 +288,13 @@ def check_sum(distro,url,check):
     sum_calc = hashlib.md5() if check == 'md5' else  hashlib.sha256()
     total_size = os.path.getsize(file_path)
     block_size = io.DEFAULT_BUFFER_SIZE
-    t = tqdm(total=total_size,unit='iB',unit_scale=True)
-    with open(file_path,'rb') as f:
+    t = tqdm(total=total_size, unit='iB', unit_scale=True)
+    with open(file_path, 'rb') as f:
         for chunk in iter(lambda: f.read(block_size ), b''):
             t.update(len(chunk))
             sum_calc.update(chunk)
     t.close()
     f.close()
-
     if sum_calc.hexdigest() in r.text:
         print('文件校验成功')
         return 0
@@ -294,7 +304,8 @@ def check_sum(distro,url,check):
         os.remove(file_path)
         sys.exit(1)
 
-def check_sum_ubuntu(distro,url):
+
+def check_sum_ubuntu(distro, url):
     r = requests.get(url)
     file_path = atilo_tmp + distro
     if not r.status_code == 200:
@@ -308,9 +319,9 @@ def check_sum_ubuntu(distro,url):
     sum_calc = hashlib.md5()
     total_size = os.path.getsize(file_path)
     block_size = io.DEFAULT_BUFFER_SIZE
-    t = tqdm(total=total_size,unit='iB',unit_scale=True)
-    with open(file_path,'rb') as f:
-        for chunk in iter(lambda: f.read(block_size),b''):
+    t = tqdm(total=total_size, unit='iB', unit_scale=True)
+    with open(file_path, 'rb') as f:
+        for chunk in iter(lambda: f.read(block_size), b''):
             t.update(len(chunk))
             sum_calc.update(chunk)
     t.close()
@@ -324,20 +335,20 @@ def check_sum_ubuntu(distro,url):
         os.remove(file_path)
         sys.exit(1)
 
+
 def clean_tmps():
     print('正在清除缓存')
     os.system('rm -rf ' + atilo_tmp + '*')
 
-def run_image(arg):
+
+def run_image(distro):
     config = load_local()
-    if not arg in config.keys():
-        print('未在本地找到' + arg + '镜像')
-        print('请先拉取该镜像')
+    if not distro in config.keys():
+        print('未在本地找到' + distro + '镜像')
         sys.exit(1)
-    distro_path = atilo_home + arg
-    infos = config.get(arg)
-    command = ''
-    command += 'proot'
+    distro_path = atilo_home + distro
+    infos = config.get(distro)
+    command = 'proot'
     command += ' --link2symlink'
     command += ' -S '
     command += distro_path
@@ -360,6 +371,7 @@ def run_image(arg):
     command += ' --login'
     os.system(command)
 
+
 def show_help():
     print('Atilo\t\t' + atilo_version )
     print('Usage: atilo [命令] [参数]\n')
@@ -371,39 +383,35 @@ def show_help():
     print('run\t\t 运行镜像')
     print('clean\t\t 清除缓存')
     print('help\t\t 帮助\n')
-    
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     check_dir()
-    
     if len(sys.argv) == 1:
         show_help()
         print('请指定一个命令')
         sys.exit(1)
-    if len(argv) > 3:
-        print('参数过多')
+    if len(sys.argv) > 3:
+        print('无用参数')
         sys.exit(1)
     if sys.argv[1] == 'help':
         show_help()
     elif sys.argv[1] == 'pull':
-        if len(sys.argv) < 3:
+        if len(sys.argv) == 2:
             print('你需要从镜像列表中指定可用镜像')
-            sys.exit(1)
-        elif len(sys.argv) >3:
-            print('无用参数')
             sys.exit(1)
         else:
             pull_image(sys.argv[2])
     elif sys.argv[1] == 'images':
         show_list()
     elif sys.argv[1] == 'remove':
-        if len(sys.argv) < 3:
+        if len(sys.argv) == 2:
             print('你需要从镜像列表中指定可用镜像')
             sys.exit(1)
         else:
             remove_image(sys.argv[2])
     elif sys.argv[1] == 'run':
-        if len(sys.argv) < 3:
+        if len(sys.argv) == 2:
             print('你需要从镜像列表中指定可用镜像')
             sys.exit(1)
         else:
@@ -413,5 +421,3 @@ if __name__ == "__main__":
     else:
         print('未知命令')
         sys.exit(1)
-    
-    
